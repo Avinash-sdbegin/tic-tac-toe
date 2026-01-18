d77d96be1d5c7da45d904d19a20d780f7cd58d23
Avinash Kumar <avinashkumarmgr24@gmail.com>
2026-01-18 21:02:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import './App.css'
+import { useState } from 'react';
+function App() {
+
+const [board, setBoard] = useState(Array(9).fill(null));
+const [isXTurn, setIsXTurn] = useState(true);
+
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+function getWinner(squares) {
+
+  for(let combination of winningCombinations) {
+    const [a, b, c] = combination;
+
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function handleSquareClick(index){
+  if(board[index] || getWinner(board)) {
+    return;
+  }
+  const updatedBoard = board.slice();
+  updatedBoard[index] = isXTurn ? 'X' : 'O';
+  setBoard(updatedBoard);
+  setIsXTurn(!isXTurn);
+}
+
+function getGameStatus(){
+  const winner = getWinner(board);
+  if(winner){
+    return `Winner: ${winner}`;
+  }
+  if(board.every(square => square !== null)){
+    return "It's a Draw!";
+  }
+  return `Next Player: ${isXTurn ? 'X' : 'O'}`;
+}
+
+function resetGame(){
+  setBoard(Array(9).fill(null));
+  setIsXTurn(true);
+}
+
+  return ( 
+   <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center">
+    {/* <div className="font-bold text-4xl text-white mb-140 bg-amber-800 w-80 py-4 flex items-center justify-center rounded-lg">TIC TAC TOE</div> */}
+    <div className="w-full max-w-md mx-5">
+        <h1 className="text-5xl font-semibold text-white text-center mb-8">TIC TAC TOE</h1>
+
+        <div className={`text-center mb-6 ${getWinner(board)?"text-2xl font-bold text-green-400 animate-bounce":"text-xl text-white"}`}>{getGameStatus()}</div>
+        <div className="grid grid-cols-3 gap-1 rounded-xl overflow-hidden mb-6">{board.map((square,index) => (
+          <button key={index} onClick={()=>handleSquareClick(index)} className={`h-32 w-full bg-gray-800 rounded-md text-6xl font-light transition-colors duration-200 hover:bg-gray-700 ${square === 'X' ? "text-white":"text-slate-400"}`}>{square}</button>
+        ))}</div>
+        <button className="w-full py-3 text-lg text-white border rounded-xl hover:bg-gray-50 hover:text-gray-800 transition-colors duration-200" onClick={resetGame}>Reset Game</button>
+        <div className="text-center mt-9 text-gray-400 text-sm "> Developed by Avinash kumar | Copyright © 2026 All rights reserved</div>
+    </div>
+    </div>
+  )
+}
+
+export default App
